4435b6077fdf38da4577c274299562e1e193cbbd
loggi <loggi.ain@gmail.com>
2021-02-12 12:57:19 +1000

    fix print details to work with subdirectory cmake files

--- cmake/avr/helpers_functions.cmake
+++ cmake/avr/helpers_functions.cmake
@@ -9,18 +9,3 @@ FUNCTION(PRINT_SIZE_OF_TARGET EXECUTABLE_NAME)
         COMMAND ${CMAKE_SIZE} -C --mcu=${MCU} ${OUTPUT_DIR}/${elf_file}	
     )
 ENDFUNCTION()
-FUNCTION(PRINT_SIZE_ALL_SOURCES EXECUTABLE_NAME)
-    set(OUTPUT_DIR ${EXECUTABLE_OUTPUT_PATH})
-    set(elf_file ${EXECUTABLE_NAME}.elf)
-    set(BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR})
-    get_target_property(SOURCES ${elf_file} SOURCES)
-    #get all sources, and make path to object files
-    list(TRANSFORM SOURCES PREPEND ${BUILD_DIR}/CMakeFiles/${elf_file}.dir/)
-    list(TRANSFORM SOURCES  APPEND ".obj")
-    #MESSAGE("OBJECT FILES = ${SOURCES}")
-    add_custom_command(TARGET ${EXECUTABLE_NAME}
-            POST_BUILD
-            COMMAND ${CMAKE_SIZE} ${SOURCES} > ${BUILD_DIR}/size_sources.txt)
-    set(PATH_TO_SIZE_UTILITY "${CURRENT_DIR}/../utils")
-    add_custom_command(TARGET ${EXECUTABLE_NAME} WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} POST_BUILD COMMAND ${PATH_TO_SIZE_UTILITY}/print_size.cmd ${BUILD_DIR}/size_sources.txt)
-ENDFUNCTION()
